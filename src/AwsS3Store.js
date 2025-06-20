dad344d389fdbec2aaaa3801050dc4535baf396f
ryantanadi <74869272+ryantanadi@users.noreply.github.com>
2025-06-20 14:48:02 +0700

    change to type module

--- src/AwsS3Store.js
+++ src/AwsS3Store.js
@@ -1,7 +1,14 @@
-const path = require('path');
-const fs = require('fs');
-const AWS = require('@aws-sdk/client-s3')
-const { S3Client, ListObjectsCommand, PutObjectCommand, HeadObjectCommand, GetObjectCommand, DeleteObjectCommand } = AWS
+import path from 'path';
+import fs from 'fs';
+import {
+  S3Client,
+  ListObjectsCommand,
+  PutObjectCommand,
+  HeadObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand
+} from '@aws-sdk/client-s3';
+
 
 class AwsS3Store {
   /**
@@ -213,7 +220,9 @@ class AwsS3Store {
   }
 }
 
-module.exports = {
-  AwsS3Store,
-  S3Client
-};
+// module.exports = {
+//   AwsS3Store,
+//   S3Client
+// };
+export { AwsS3Store };
+export { S3Client } from '@aws-sdk/client-s3';
